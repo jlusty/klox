8c23ce8d832eaa3f7b66fecb64d3a4e5cb0095eb
John Lusty <54030459+jlusty@users.noreply.github.com>
2021-09-04 03:02:39 +0100

    Add initial generator (with instructions)

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ klox is an interpreter for the Lox language (from the book "Crafting Interpreter
 
 Running `./gradlew run --console=plain` will allow you to use the Lox REPL.
 Running `./gradlew run --args='C:\some\absolute\path\to\file.txt'` will allow you to run Lox on a file.
+
+## Running code generation tool
+
+Running `kotlinc -script src/main/kotlin/business/plants/tool/GenerateAst.kts path/to/output/file.txt` will run the code generation script.
--- src/main/kotlin/business/plants/tool/GenerateAst.kts
+++ src/main/kotlin/business/plants/tool/GenerateAst.kts
@@ -1,12 +1,44 @@
 package business.plants.tool
 
+import java.io.PrintWriter
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
-    if (args.size != 1) {
-        println("Usage: generate_ast <output directory>")
-        exitProcess(64)
+if (args.size != 1) {
+    println("Usage: generate_ast <output directory>")
+    exitProcess(64)
+}
+val outputDir: String = args[0]
+defineAst(
+    outputDir, "Expr", listOf(
+        "Binary - left: Expr, operator: Token, right: Expr",
+        "Grouping - expression: Expr",
+        "Literal - value: Any",
+        "Unary - operator: Token, right: Expr",
+    )
+)
+
+fun defineAst(outputDir: String, baseName: String, types: List<String>) {
+    val path: String = "$outputDir/$baseName.kt"
+    val writer = PrintWriter(path, "UTF-8")
+
+    writer.println("package business.plants.klox")
+    writer.println()
+    writer.println("abstract class $baseName {")
+    writer.println("    companion object {")
+
+    // The AST classes
+    for (type in types) {
+        val className = type.split("-")[0].trim()
+        val fields = type.split("-")[1].trim()
+        defineType(writer, baseName, className, fields)
     }
-    val outputDir = args[0]
-    println(outputDir)
+
+    writer.println("    }")
+    writer.println("}")
+    writer.close()
+}
+
+fun defineType(writer: PrintWriter, baseName: String, className: String, fieldList: String) {
+    writer.println("        class $className($fieldList) : $baseName() {")
+    writer.println("        }")
 }
