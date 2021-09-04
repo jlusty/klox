035d93971c5e17b7ece227adb92657082d203e06
John Lusty <54030459+jlusty@users.noreply.github.com>
2021-09-04 23:27:31 +0100

    Comment out AstPrinter

--- src/main/kotlin/business/plants/klox/AstPrinter.kt
+++ src/main/kotlin/business/plants/klox/AstPrinter.kt
@@ -1,49 +1,49 @@
-package business.plants.klox
-
-class AstPrinter : Expr.Visitor<String> {
-    fun print(expr: Expr): String {
-        return expr.accept(this)
-    }
-
-    override fun visitBinaryExpr(expr: Expr.Binary): String {
-        return parenthesize(expr.operator.lexeme, expr.left, expr.right)
-    }
-
-    override fun visitGroupingExpr(expr: Expr.Grouping): String {
-        return parenthesize("group", expr.expression)
-    }
-
-    override fun visitLiteralExpr(expr: Expr.Literal): String {
-        if (expr.value == null) return "nil"
-        return expr.value.toString()
-    }
-
-    override fun visitUnaryExpr(expr: Expr.Unary): String {
-        return parenthesize(expr.operator.lexeme, expr.right)
-    }
-
-    // TODO: Check this method
-    override fun visitVariableExpr(expr: Expr.Variable): String {
-        return parenthesize("var", expr)
-    }
-
-    private fun parenthesize(name: String, vararg exprs: Expr): String {
-        return "($name ${(exprs.map { expr -> expr.accept(this) }).joinToString()})"
-    }
-}
-
-// TODO: Delete this, not required in future
-fun main(args: Array<String>) {
-    val expression: Expr = Expr.Binary(
-        Expr.Unary(
-            Token(TokenType.MINUS, "-", null, 1),
-            Expr.Literal(123)
-        ),
-        Token(TokenType.STAR, "*", null, 1),
-        Expr.Grouping(
-            Expr.Literal(45.67)
-        )
-    )
-
-    println(AstPrinter().print(expression))
-}
+//package business.plants.klox
+//
+//class AstPrinter : Expr.Visitor<String> {
+//    fun print(expr: Expr): String {
+//        return expr.accept(this)
+//    }
+//
+//    override fun visitBinaryExpr(expr: Expr.Binary): String {
+//        return parenthesize(expr.operator.lexeme, expr.left, expr.right)
+//    }
+//
+//    override fun visitGroupingExpr(expr: Expr.Grouping): String {
+//        return parenthesize("group", expr.expression)
+//    }
+//
+//    override fun visitLiteralExpr(expr: Expr.Literal): String {
+//        if (expr.value == null) return "nil"
+//        return expr.value.toString()
+//    }
+//
+//    override fun visitUnaryExpr(expr: Expr.Unary): String {
+//        return parenthesize(expr.operator.lexeme, expr.right)
+//    }
+//
+//    // TODO: Check this method
+//    override fun visitVariableExpr(expr: Expr.Variable): String {
+//        return parenthesize("var", expr)
+//    }
+//
+//    private fun parenthesize(name: String, vararg exprs: Expr): String {
+//        return "($name ${(exprs.map { expr -> expr.accept(this) }).joinToString()})"
+//    }
+//}
+//
+//// TODO: Delete this, not required in future
+//fun main(args: Array<String>) {
+//    val expression: Expr = Expr.Binary(
+//        Expr.Unary(
+//            Token(TokenType.MINUS, "-", null, 1),
+//            Expr.Literal(123)
+//        ),
+//        Token(TokenType.STAR, "*", null, 1),
+//        Expr.Grouping(
+//            Expr.Literal(45.67)
+//        )
+//    )
+//
+//    println(AstPrinter().print(expression))
+//}
